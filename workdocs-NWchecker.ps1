d147d20abfc87173e95e7608823a9cdcdc06ede1
Yoichi Takizawa <yo1@tkywave.com>
2016-09-18 23:22:52 +0900

    Code by Stop-Process

---
+++ workdocs-NWchecker.ps1
@@ -0,0 +1,46 @@
+﻿# Set WiFi-SSID, you want to stop WorkDocs 
+$ssid = "yo1-mobile"
+
+# Set Checking interval for WiFi-SSID
+$interval = 60
+
+
+# Set Environment for WorkDocs
+$WorkDocs = "\Programs\Amazon.com, Inc\Amazon WorkDocs.appref-ms"
+$WorkDocsPath = [Environment]::GetFolderPath('StartMenu') + $WorkDocs
+$WorkDocsProcessName = "AmazonWorkDocs"
+$LogMessage = ""
+
+# Main Process
+while (1) {
+    # Get now connect WiFi-SSID
+    $netsh_ssid = netsh wlan show interface | Select-String "    SSID                   :"
+    $now_ssid = $netsh_ssid -replace ".*: "
+    
+    # Get Now time for log
+    $nowtime = Get-Date -Format "yyyy/MM/dd-HH:mm:ss"
+
+    if ( $now_ssid -eq $ssid ) {
+    　　　　# Process of Stop WorkDocs
+        if ( ( Get-Process $WorkDocsProcessName -ErrorAction 0 )  ) {
+            Stop-Process -name $WorkDocsProcessName -ErrorAction 0
+        }
+        $LogMessage = "Now SSID is " + $now_ssid +". WorkDocs stopped."
+
+    } else {
+        # Process of Start WorkDocs
+        if ( ( Get-Process $WorkDocsProcessName -ErrorAction 0 )  ) {
+            $LogMessage = "WorkDocs is still running."
+        } else {
+            Start-Process $WorkDocsPath
+            $LogMessage = "Now SSID is " + $now_ssid +". WorkDocs is starting."
+        }
+    }
+
+    # Oputput Log
+    Write-Output ($nowtime + " " + $LogMessage)
+
+    # Sleep for several seconds
+    Start-Sleep -Seconds $interval
+}
+
